f49d08973162db5bcebae7a8328f211c1c9962cd
Eugene Trounev <eugene.trounev@gmail.com>
2014-12-04 16:29:11 -0500

    load function
    A stub to load all required modules when needed

--- src/tlc-base.lua
+++ src/tlc-base.lua
@@ -1,4 +1,21 @@
+local _c=function(b,c)for d,e in pairs(c)do b[d]=e end;return b end
 local tlc = {}
 
+function tlc:load(modules, baseDir)
+  baseDir = baseDir or "lib.tlc."
+  modules = modules or {
+    "tlc-logic",
+	  "tlc-math",
+	  "tlc-misc",
+	  "tlc-string",
+	  "tlc-tables",
+	  "tlc-types"
+  }
+  for i, name in ipairs(modules) do
+    local module = reqire (baseDir .. name)
+    _c(self, module)
+  end
+  return self
+end
 
 return tlc
