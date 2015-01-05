9a50bc23ce458f656a45b5d6dbd40c4f2e37bc44
Eugene Trounev <eugene.trounev@gmail.com>
2015-01-04 20:25:54 -0500

    Dynamic module loading system

--- src/tlc-base.lua
+++ src/tlc-base.lua
@@ -3,16 +3,16 @@ local tlc = {}
 
 function tlc:load(modules, baseDir)
   baseDir = baseDir or "lib.tlc."
-  modules = modules or {
-    "tlc-logic",
-	  "tlc-math",
-	  "tlc-misc",
-	  "tlc-string",
-	  "tlc-tables",
-	  "tlc-types"
-  }
+--   modules = modules or {
+--     "tlc-logic",
+--     "tlc-math",
+--     "tlc-misc",
+--     "tlc-string",
+--     "tlc-tables",
+--     "tlc-types"
+--   }
   for i, name in ipairs(modules) do
-    local module = reqire (baseDir .. name)
+    local module = require (baseDir .. name)
     _c(self, module)
   end
   return self
